2b4ce17b671e4b5ebeb438ef9e3b0de4124ef534
sathishkumaranandh-alt <sathishkumaranandh@gmail.com>
2026-09-18 23:09:41 +0530

    Add Subscription component with plan options

---
+++ src/pages/Subscription.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Subscription() {
+  return (
+    <div>
+      <h2>Choose a Subscription Plan</h2>
+      <button>Monthly Plan</button>
+      <button>Yearly Plan</button>
+    </div>
+  );
+}
+
+export default Subscription;
