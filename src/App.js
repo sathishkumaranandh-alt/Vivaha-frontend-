779e8d1773b30dbbcfc07956cf62e2910ba60510
sathishkumaranandh-alt <sathishkumaranandh@gmail.com>
2026-09-18 22:30:11 +0530

    Refactor App component to use React Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from "react";
-import Row from "./Row";
-import Banner from "./Banner";
-import Navbar from "./Navbar";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import ProfileSearch from "./pages/ProfileSearch";
+import Matches from "./pages/Matches";
+import Recommendations from "./pages/Recommendations";
+import Subscription from "./pages/Subscription";
+import SubscriptionDashboard from "./pages/SubscriptionDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminAnalytics from "./pages/AdminAnalytics";
 
 function App() {
   return (
-    <div className="app">
+    <Router>
       <Navbar />
-      <Banner />
-      <Row title="Trending Now" fetchUrl="/trending/all/week" />
-      <Row title="Top Rated" fetchUrl="/movie/top_rated" />
-      <Row title="Tamil Movies" fetchUrl="/discover/movie?with_original_language=ta" />
-      <Row title="Romance" fetchUrl="/discover/movie?with_genres=10749" />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<ProfileSearch />} />
+        <Route path="/matches" element={<Matches />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/subscription-dashboard" element={<SubscriptionDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin-analytics" element={<AdminAnalytics />} />
+      </Routes>
+    </Router>
   );
 }
 
