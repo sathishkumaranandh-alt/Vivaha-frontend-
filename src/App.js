dc64a7255578f9d7324c2697f797e6ec8c110103
sathishkumaranandh-alt <sathishkumaranandh@gmail.com>
2026-09-19 20:02:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navigation";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 // Pages
