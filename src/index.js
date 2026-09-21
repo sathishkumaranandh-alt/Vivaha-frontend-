73078df2a3079cb80fa3f04f48e8f0cf87ee09e7
sathishkumaranandh-alt <sathishkumaranandh@gmail.com>
2026-09-21 17:39:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./styles/global.css";
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
+import ToastContainer from "./components/ToastContainer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
+      <ToastContainer />
     </ErrorBoundary>
   </React.StrictMode>
 );
